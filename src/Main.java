a5e555a984439dcb0ba0c65bee5148f688e8fc47
Admin <b1oveecsgo@gmail.com>
2024-09-03 20:44:18 +0300

    исправлено

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -8,39 +10,48 @@ public class Main {
         int clientDeviceYear = 2014;
         checkDevice(clientOS, clientDeviceYear);
 
-        int deliveryDistance = 95;
-        delivery(deliveryDistance);
+        int deliveryDistance = 155;
+        int deliveryDays = checkDeliveryDays(deliveryDistance);
+        System.out.println("Потребуется дней: " + deliveryDays);
     }
-    public static void checkYear( int y){
 
-        if ((y%4 == 0 && y % 100 !=0)|| y % 400 == 0)
+    public static void checkYear(int y) {
+
+        if ((y % 4 == 0 && y % 100 != 0) || y % 400 == 0)
             System.out.printf("%d год является високосным\n", y);
         else
             System.out.printf("%d год не является високосным\n", y);
     }
-    public static void checkDevice(byte a, int y){
-        if (a == 0){
-            if (y < 2015)
+
+    public static void checkDevice(byte a, int y) {
+        int currentYear = LocalDate.now().getYear();
+        if (a == 0) {
+            if (y < currentYear)
                 System.out.println("Установите облегченную версию приложения для iOS по ссылке");
             else
                 System.out.println("Установите версию приложения для iOS по ссылке");
-        }
-        else{
-            if (y < 2015)
+        } else {
+            if (y < currentYear)
                 System.out.println("Установите облегченную версию приложения для Android по ссылке");
             else
                 System.out.println("Установите версию приложения для Android по ссылке");
         }
     }
-    public static void delivery(int a){
+
+    public static int checkDeliveryDays(int a) {
         int days = 1;
-        if (a < 20)
-            System.out.println("Потребуется дней:" + days);
-        else if (a >= 20 && a < 60)
-            System.out.println("Потребуется дней:" + (days+1));
-        else if (a >= 60 && a < 100)
-            System.out.println("Потребуется дней:" + (days+2));
-        else
-            System.out.println("Доставки нет");
+        if (a < 20) {
+            return days;
+        } else if (a >= 20 && a < 60) {
+            return days += 1;
+        } else if (a >= 60 && a < 100) {
+            return days += 2;
+        }
+        else {
+            return days=0;
+        }
+
+
     }
+
 }
